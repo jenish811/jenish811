c0f445665059878434d4ce977c8378ddd37e0443
A2D-virang-khunt <virang.khunt@a2d.co.in>
2024-05-25 17:42:25 +0530

    Set Logo

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Weather app',
+      title: 'Weather',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
